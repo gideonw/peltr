b01babd3d6aaa92f4f3aa9c010330dd181e35b86
Dana Burkart <dana.burkart@gmail.com>
2022-12-08 15:31:48 -0800

    Adds a generic Message struct which can wrap all other messages.

--- pkg/proto/messages.go
+++ pkg/proto/messages.go
@@ -2,8 +2,53 @@ package proto
 
 import (
 	"bytes"
+	"encoding/binary"
 	"encoding/gob"
+	"io"
 )
+
+type MessageType uint8
+
+// The Message type wraps all messages sent between workers and servers
+type Message struct {
+	Type MessageType
+	Data []byte
+}
+
+func (m *Message) Read(reader io.Reader) error {
+	lengthPrefix := make([]byte, 4)
+	err := binary.Read(reader, binary.LittleEndian, &lengthPrefix)
+	if err != nil {
+		return err
+	}
+
+	messageLength := binary.LittleEndian.Uint32(lengthPrefix)
+	messageBuffer := make([]byte, messageLength)
+	_, err = io.ReadFull(reader, messageBuffer)
+	if err != nil {
+		return err
+	}
+
+	m.Type = MessageType(messageBuffer[0])
+	m.Data = messageBuffer[1:]
+
+	return nil
+}
+
+func (m *Message) Write(writer io.Writer) error {
+	// 1 byte for MessageType, and 4 bytes for message length
+	b := make([]byte, len(m.Data)+1+4)
+	binary.LittleEndian.PutUint32(b, uint32(len(m.Data)+1))
+	b[4] = byte(m.Type)
+	copy(b[5:], m.Data)
+
+	_, err := writer.Write(b)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
 
 type Identify struct {
 	ID       string
